b12b550d235714fb1d6472fcf868a6959750d518
AhogeK <ahogek@gmail.com>
2024-04-16 19:35:43 +0800

    feat: add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}', // 添加对 .vue 文件的支持
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
